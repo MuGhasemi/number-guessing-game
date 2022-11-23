fb45a96c322bea9e7b872ef3792b1ca544219d12
mughasemi <mughasemi2008@gmail.com>
2022-11-23 19:45:49 +0330

    Updated and debug file

--- numberGuessing.py
+++ numberGuessing.py
@@ -4,14 +4,20 @@ print("Guess Between The <1 - 10>")
 number = random.randint(1 , 10)
 
 while True:
-    guess = input("Enter Your Guess Number : ")
+    guess = input("-Enter Your Guess Number : ")
     try :
-        if int(guess) > number :
+        if int(guess) > 10 or int(guess) <= 0:
+            print("Please Enter Number Between <1 - 10>")
+        elif int(guess) > number :
             print("Higher Guess ...")
         elif int(guess) < number :
             print("Lower Guess ...")
         else :
             print(f"You Win , Guess : {number}")
-            break
+            guess = input("-Would You Like Try Again (yes or no)?  ")
+            if guess == "yes" or guess == "y" :
+                number = random.randint(1 , 10)
+            elif guess == "no" or guess == "n":
+                break
     except ValueError:
         print("Please Enter Number !!" )
